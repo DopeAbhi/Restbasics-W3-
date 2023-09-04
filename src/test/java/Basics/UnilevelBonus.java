42f2a6a83d781c6972b361e3157e3749cc35841d
DopeAbhi <97584507+DopeAbhi@users.noreply.github.com>
2023-09-04 20:53:53 +0530

    4sep 8:53

--- src/test/java/Basics/UnilevelBonus.java
+++ src/test/java/Basics/UnilevelBonus.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.given;
 public class UnilevelBonus {
 
     public static void main(String[] args) {
-        String token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5MzgzMTgzMCwiZXhwIjoxNjk2NDIzODMwfQ.l22R1tUXa9H0y924T2pk8AOLx9lPLyXkMwLxthEQOsE";
+        String token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5MzU4NDc1NiwiZXhwIjoxNjk2MTc2NzU2fQ.WC6eeH0Q7kgnqBamMXNKjd32qVtm8v7EDFTfum3y1lQ";
 
         RestAssured.baseURI="https://quickdev3.super.one";
 
@@ -20,7 +20,7 @@ public class UnilevelBonus {
         Date date=new Date();
         long end=date.getTime();
      String rewardresp=   given().header("device-type","WEB").header("token",token)
-                .queryParams("skips",0,"start",0,"end",""+end+"","page_num",25,"memberPackageId","")
+                .queryParams("skips",0,"start",0,"end",""+end+"","page_num",25,"memberPackageId","116783")
                 .when().get("/reader/admin/getPayoutsList")
                 .then().log().all().assertThat().statusCode(200).extract().response().asString();
         JsonPath rewardjson=Reuseablemethods.rawtojson(rewardresp);
@@ -45,11 +45,13 @@ public class UnilevelBonus {
      JsonPath searchjson=Reuseablemethods.rawtojson(searchresp);
      String highestpack=searchjson.getString("data.details[0].userRank");
      String email=searchjson.getString("data.details[0].email");
+           System.out.println("UB "+j+"");
            System.out.println(profiteer);
            System.out.println(highestpack);
            System.out.println(email);;
            System.out.println(rewardtype);
-           System.out.println(reward);
+           System.out.println(reward+"\n");
+
            j++;
 
         }while (rewardtype.equalsIgnoreCase("UNILEVEL"));
