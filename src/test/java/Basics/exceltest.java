f1d24a49bacbf6624d16c3bb5d2c526a1027a68b
DopeAbhi <rahulonlinetutor@gmail.com>
2023-10-09 17:01:09 +0530

    9 oct

--- src/test/java/Basics/exceltest.java
+++ src/test/java/Basics/exceltest.java
@@ -8,6 +8,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.Iterator;
 
 public class exceltest {
@@ -17,6 +18,8 @@ public class exceltest {
         FileInputStream fis = new FileInputStream("/home/abhay/Restbasics-W3-/src/test/java/resources/Superone.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
         int sheets = workbook.getNumberOfSheets();
+      String [] userdata = new String[4];
+
 
 
         for (int i = 0; i < sheets; i++) {
@@ -27,20 +30,30 @@ public class exceltest {
 
                 Iterator<Row> rows = sheet.iterator();// sheet is collection of rows
                 Row firstrow = rows.next();
-                Iterator<Cell> ce=firstrow.cellIterator();
+                Row secondrow = rows.next();
+
+                Iterator<Cell> ce=secondrow.cellIterator();
                 int k=0;
                 int coloumn = 0;
                 while(ce.hasNext())
                 {
                     Cell value=ce.next();
 
-                    if(value.getStringCellValue().equalsIgnoreCase("password"))
-                    {
-                        System.out.println(value.getStringCellValue());
-                        coloumn=k;
-                        System.out.println(coloumn);
+                    userdata[k]=value.getStringCellValue();
 
-                    }
+                    System.out.println(userdata[k]);
+              //      System.out.println(value.getStringCellValue());
+
+
+//                    if(value.getStringCellValue().equals("User"))
+//                    {
+//                        System.out.println(value.getStringCellValue());
+//                        coloumn=k;
+//                        System.out.println(coloumn);
+//                        break;
+//
+//                    }
+
 
                     k++;
                 }
