96f3ebff9dece5d5a18543440e95d7b98a858e4d
DopeAbhi <rahulonlinetutor@gmail.com>
2023-10-09 17:34:41 +0530

    9 oct

--- src/test/java/Basics/exceltest.java
+++ src/test/java/Basics/exceltest.java
@@ -1,5 +1,8 @@
 package Basics;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import net.openhft.chronicle.core.values.StringValue;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -11,6 +14,8 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Iterator;
 
+import static io.restassured.RestAssured.given;
+
 public class exceltest {
     public static void main(String[] args) throws IOException {
 
@@ -18,8 +23,7 @@ public class exceltest {
         FileInputStream fis = new FileInputStream("/home/abhay/Restbasics-W3-/src/test/java/resources/Superone.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
         int sheets = workbook.getNumberOfSheets();
-      String [] userdata = new String[4];
-
+        String[] userdata = new String[4];
 
 
         for (int i = 0; i < sheets; i++) {
@@ -29,39 +33,62 @@ public class exceltest {
 
 
                 Iterator<Row> rows = sheet.iterator();// sheet is collection of rows
-                Row firstrow = rows.next();
-                Row secondrow = rows.next();
+                while (rows.hasNext()) {
+                    Row firstrow = rows.next();
+                    Row secondrow = rows.next();
 
-                Iterator<Cell> ce=secondrow.cellIterator();
-                int k=0;
-                int coloumn = 0;
-                while(ce.hasNext())
-                {
-                    Cell value=ce.next();
+                    Iterator<Cell> ce = secondrow.cellIterator();
+                    int k = 0;
+                    int coloumn = 0;
+                    while (ce.hasNext()) {
+                        Cell value = ce.next();
+                        if (k<2) {
 
-                    userdata[k]=value.getStringCellValue();
+                            userdata[k] = value.getStringCellValue();
 
-                    System.out.println(userdata[k]);
-              //      System.out.println(value.getStringCellValue());
-
-
-//                    if(value.getStringCellValue().equals("User"))
-//                    {
-//                        System.out.println(value.getStringCellValue());
-//                        coloumn=k;
-//                        System.out.println(coloumn);
-//                        break;
-//
-//                    }
+                            System.out.println(userdata[k]);
+                        }
+                        else {
+                            userdata[k]=String.valueOf(value.getNumericCellValue());
+                            System.out.println(userdata[k]);
+                        }
 
 
-                    k++;
+
+                        RestAssured.baseURI="https://quickdev3.super.one";
+
+
+                        //Intiate Login
+                        given().header("device-type", "WEB")
+                                .body(usertreepayload.intiateloginpayload(userdata[0]))
+                                .when().patch("/writer/v2/user/email/initiatelogin")
+                                .then().assertThat().statusCode(200);
+                        //Login
+
+                        String loginresp = given().header("device-type", "WEB")
+                                .body(usertreepayload.loginpayload(userdata[0], userdata[1]))
+                                .when().patch("/writer/user/email/login")
+                                .then().assertThat().statusCode(200).extract().response().asString();
+                        JsonPath loginjson = Reuseablemethods.rawtojson(loginresp);
+                        String token = loginjson.getString("data.token");
+                        System.out.println(token);
+
+
+                         given().header("token", token).header("device-type", "WEB")
+                                .body(usertreepayload.pack_purchase_payload(Integer.parseInt(userdata[2])))
+                                .when().patch("/writer/member/package/103138").
+                                then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+
+
+
+                        k++;
+                    }
+
+
                 }
 
-
-
             }
-
         }
     }
 }
