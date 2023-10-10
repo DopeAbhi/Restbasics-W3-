d7e38f361ee7fcbbfe682440fb13e96b93db8931
DopeAbhi <rahulonlinetutor@gmail.com>
2023-10-10 10:51:11 +0530

    10oct

--- src/test/java/Basics/Packpurchase.java
+++ src/test/java/Basics/Packpurchase.java
@@ -1,14 +1,23 @@
 package Basics;
 
+
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
-import java.util.Scanner;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Iterator;
 
 import static io.restassured.RestAssured.given;
 
-public class Packpurchase extends  exceltest {
+public class Packpurchase {
 
     /*
     1-Entry
@@ -18,41 +27,68 @@ public class Packpurchase extends  exceltest {
     7-Platinum
     9-Diamond
     */
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Parent Email Address");
-        String parentemail=scanner.next();
-        System.out.println("Parent password");
-        String parentpassword=scanner.next();
-        System.out.println("Pack number");
-        int packnumber=scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        String[] userdata = new String[4];
+        FileInputStream fis = new FileInputStream("/home/abhay/Restbasics-W3-/src/test/java/resources/Superone.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        int sheets = workbook.getNumberOfSheets();
 
 
-        RestAssured.baseURI="https://quickdev3.super.one";
+
+        for (int i = 0; i < sheets; i++) {
+
+            if (workbook.getSheetName(i).equalsIgnoreCase("Packpurchase")) {
+                XSSFSheet sheet = workbook.getSheetAt(i);
 
 
-            //Intiate Login
-            given().header("device-type", "WEB")
-                    .body(usertreepayload.intiateloginpayload(parentemail))
-                    .when().patch("/writer/v2/user/email/initiatelogin")
-                    .then().assertThat().statusCode(200);
-            //Login
+                Iterator<Row> rows = sheet.iterator();// sheet is collection of rows
+                rows.next();
+                while (rows.hasNext()) {
+                    Row row = rows.next();
 
-            String loginresp = given().header("device-type", "WEB")
-                    .body(usertreepayload.loginpayload(parentemail, parentpassword))
-                    .when().patch("/writer/user/email/login")
-                    .then().assertThat().statusCode(200).extract().response().asString();
-            JsonPath loginjson = Reuseablemethods.rawtojson(loginresp);
-            String token = loginjson.getString("data.token");
-            System.out.println(token);
+                    Iterator<Cell> ce = row.cellIterator();
+                    int k = 0;
+                    while (ce.hasNext()) {
+                        Cell value = ce.next();
+                        if (k<2) {
 
-            String purchaseresp = given().header("token", token).header("device-type", "WEB")
-                    .body(usertreepayload.pack_purchase_payload(packnumber))
-                    .when().patch("/writer/member/package/103138").
-                    then().log().all().assertThat().statusCode(200).extract().response().asString();
+                            userdata[k] = value.getStringCellValue();
 
+                            System.out.println(userdata[k]);
+                        }
+                        else {
+                            userdata[k]=String.valueOf(value.getNumericCellValue());
+                            System.out.println(userdata[k]);
+                        }
+                        k++;
+                    }
+                    RestAssured.baseURI="https://quickdev3.super.one";
+                    //Intiate Login
+                    given().header("device-type", "WEB")
+                            .body(usertreepayload.intiateloginpayload(userdata[0]))
+                            .when().patch("/writer/v2/user/email/initiatelogin")
+                            .then().log().all().assertThat().statusCode(200);
+                    //Login
+
+                    String loginresp = given().header("device-type", "WEB")
+                            .body(usertreepayload.loginpayload(userdata[0], userdata[1]))
+                            .when().patch("/writer/user/email/login")
+                            .then().assertThat().statusCode(200).extract().response().asString();
+                    System.out.println(loginresp);
+                    JsonPath loginjson = Reuseablemethods.rawtojson(loginresp);
+                    String token = loginjson.getString("data.token");
+                    System.out.println(token);
+
+                    String purchaseresp = given().header("token", token).header("device-type", "WEB")
+                            .body(usertreepayload.pack_purchase_payload(
+                                    (int)Double.parseDouble(userdata[2])))
+                            .when().patch("/writer/member/package/103138").
+                            then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+                }
+
+            }
 
         }
-
-    }
-
+    }}
